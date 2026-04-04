9ca5a6622708fff6a93995db91a8c461d885fb39
ArjenPXL <116876154+ArjenPXL@users.noreply.github.com>
2026-04-04 22:03:41 +0200

    fallbaclk when 404

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,11 @@ import App from './App.vue';
 import router from './router';
 import './styles.css';
 
+const redirectPath = sessionStorage.getItem('redirectPath');
+
+if (redirectPath) {
+  sessionStorage.removeItem('redirectPath');
+  window.history.replaceState(null, '', redirectPath);
+}
+
 createApp(App).use(router).mount('#app');
